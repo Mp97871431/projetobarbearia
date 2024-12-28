095bcb10f83890af47490bf94cfce2ca117ad743
Mp97871431 <carlos.rocha9787@gmail.com>
2024-12-28 14:19:17 -0300

    atualizações

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function mudouTamanho(){
+    if(window.innerwidth >=768){
+        itens.style.display = 'block'
+    } else{
+        intens.style.display = 'none'
+    }
+}
+
+
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
