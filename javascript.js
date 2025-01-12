29b1ff6b43796cb940e2368266b8091e01b88f28
Mp97871431 <carlos.rocha9787@gmail.com>
2025-01-12 10:14:48 -0300

    atualizando

--- javascript.js
+++ javascript.js
@@ -14,3 +14,18 @@ function clickMenu(){
         itens.style.display = 'block'
     }
 }
+
+function clicar(){
+   var res = window.document.getElementById('res')
+   res.innerHTML = `<p> corte de barba modelo ingles que traz muito mais elegancia para o seu dia a dia</p>`
+}
+
+function clicar2(){
+    var res2 = window.document.getElementById('res2')
+    res2.innerHTML = `<p>corte estilo vintagi, bem retro porém com muita elegancia para o seu dia a dia</p>`
+ }
+
+ function clicar3(){
+    var res3 = window.document.getElementById('res3')
+    res3.innerHTML = `<p>corte estilo vintagi, bem retro porém com muita elegancia para o seu dia a dia</p>`
+ }
